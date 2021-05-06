6dc941b282d4e042440d3df308b0700738115754
Ann Cascarano <a.cascarano@gmail.com>
2021-05-05 21:40:47 -0400

    Testing things out

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome, pull up a seat and grab a cup!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Hey Hey Hey! Welcome to my first next.js app. This is pretty cool.
         </p>
       </main>
 
